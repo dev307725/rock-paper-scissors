c5726ddab6e14547d19d338c38704388f9eaba1f
Dylan Villanueva <105649094+dev307725@users.noreply.github.com>
2022-05-20 09:51:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -32,20 +32,20 @@ hand = [rock, paper, scissors]
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors.\n"))
 
 if user_choice < 0 or user_choice > 2:
-  print("You typed an invalid number.")
+    print("You typed an invalid number.")
 else:
-  print(hand[user_choice])
+    print(hand[user_choice])
   
-  ai_choice = random.randint(0, 2)
-  print(f"Computer chose:\n{hand[ai_choice]}")
+    ai_choice = random.randint(0, 2)
+    print(f"Computer chose:\n{hand[ai_choice]}")
   
-  if user_choice == 0 and ai_choice == 2:
-    print("You win!")
-  elif user_choice == 2 and ai_choice == 0:
-    print("You lose!")
-  elif user_choice == ai_choice:
-    print("It's a tie!")
-  elif user_choice > ai_choice:
-    print("You win!")
-  elif user_choice < ai_choice:
-    print("You lose!")
+    if user_choice == 0 and ai_choice == 2:
+        print("You win!")
+    elif user_choice == 2 and ai_choice == 0:
+        print("You lose!")
+    elif user_choice == ai_choice:
+        print("It's a tie!")
+    elif user_choice > ai_choice:
+        print("You win!")
+    elif user_choice < ai_choice:
+        print("You lose!")
